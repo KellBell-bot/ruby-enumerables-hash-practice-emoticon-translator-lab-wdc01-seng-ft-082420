234eb6d5b0cd141d53e00b5e22d4ed90f2382c6c
Kellie Hamilton <kl.hamilton13@gmail.com>
2020-08-18 12:45:55 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,12 +3,30 @@ require 'pry'
 # require modules here
 
 def load_library(file_path)
- emoticon_hash= YAML.load_file(file_path)
-    binding.pry
+  new_array = {:meaning=> {}, :emoji=> {}}
+ emoji_library= YAML.load_file(file_path)
+ emoji_library.map do |key, values|
+   new_array[:meaning][values[1]]= key
+   #binding.pry
+   new_array[:emoji][values[0]]= values[1]
+   #binding.pry
+ end
+ new_array
 end
 
-def get_japanese_emoticon(emoticon_hash)
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  new_array= load_library(file_path)
+  translated = nil
+  new_array[:emoji].each do |english_emoji, japanese_emoji|
+    if emoticon == english_emoji
+      translated = japanese_emoji
+    end
+  end
+  if translated == nil
+    return "Sorry, that emoticon was not found"
+  else
+    return translated
+end
 end
 
 def get_english_meaning
